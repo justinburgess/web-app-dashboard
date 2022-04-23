f07060b017c5458fae9a0be4dcd1633bf33a4969
justinburgess <signalincode@gmail.com>
2022-04-23 10:26:22 -0400

    completed step 6 in study guide

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@ const alertBanner = document.getElementById('alert');
 const trafficCanvas = document.getElementById('traffic-chart');
 const dailyCanvas = document.getElementById("daily-chart");
 const mobileCanvas = document.getElementById("mobile-chart");
+const user = document.getElementById("userField");
+const message = document.getElementById("messageField");
+const send = document.getElementById("send");
+
 
 let trafficData = {
     labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
@@ -119,3 +123,18 @@ alertBanner.addEventListener('click', e => {
         alertBanner.style.display = "none";
     }
 });
+
+// send events
+
+send.addEventListener('click', () => {
+    // ensure user and message fields are filled out
+    if (user.value === "" && message.value === "") {
+    alert("Please fill out user and message fields before sending");
+    } else if (user.value === "" ) {
+    alert("Please fill out user field before sending");
+    } else if (message.value === "" ) {
+    alert("Please fill out message field before sending");
+    } else {
+    alert(`Message successfully sent to: ${user.value}`);
+    }
+    });
