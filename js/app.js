45c0759983aac747343642b4e43fc199039ab2de
justinburgess <signalincode@gmail.com>
2022-04-30 06:57:21 -0400

    finished day

--- js/app.js
+++ js/app.js
@@ -112,7 +112,7 @@ alertBanner.innerHTML =
     `
         <div class="alert-banner>
             <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete</p>
-            <p class="alert-banner-close">x</p>
+            <p class="">x</p>
         </div>
     `
 ;
@@ -121,6 +121,8 @@ alertBanner.addEventListener('click', e => {
     const element = e.target;
     if (element.classList.contains("alert-banner-close")) {
         alertBanner.style.display = "none";
+    } else {
+        alertBanner.style.display = "block";
     }
 });
 
